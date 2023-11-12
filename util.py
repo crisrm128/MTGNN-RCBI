0986abad59f9fb4d677e1a32fdba5ded79b07536
crisrm128 <crm128@gcloud.ua.es>
2023-11-12 18:11:26 +0100

    Debuf util.py train-val-test shapes v3

--- util.py
+++ util.py
@@ -29,8 +29,9 @@ class DataLoaderS(object):
         # print("Tmp: ", tmp)
         # print("Tmp shape: ", tmp.shape)
         print(f"train shape: {len(self.train)} {len(self.train[0])}")
-        print(f"train shape: {len(self.valid)} {len(self.valid[0])}")
-        print(f"train shape: {len(self.test)} {len(self.test[0])}")
+        print(f"valid shape: {len(self.valid)} {len(self.valid[0])}")
+        print(f"test shape: {len(self.test)} {len(self.test[0])}")
+        print(self.test)
 
         self.scale = self.scale.to(device)
         self.scale = Variable(self.scale)
